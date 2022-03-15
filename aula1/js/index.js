f925965e63cc8dc363d2bcfd930c49b3bae827c3
Juliano Cesar Magoga <49631397+JulianoCesarM@users.noreply.github.com>
2022-03-15 16:13:53 -0300

    new

--- aula1/js/index.js
+++ aula1/js/index.js
@@ -1,12 +1,12 @@
 function Calcular(){
-
-    var np1 = document.getElementById("np1");
-    var np2 = document.getElementById("np2");
-    var part1 = document.getElementById("part1");
-    var part2 = document.getElementById("part2");
-    var pluri = document.getElementById("pluri");
+    var np1 = Number.parseFloat(window.document.getElementById("np1").value);
+    var np2 = Number.parseFloat(window.document.getElementById("np2").value);
+    var part1 = Number.parseFloat(window.document.getElementById("part1").value);
+    var part2 = Number.parseFloat(window.document.getElementById("part2").value);
+    var pluri = Number.parseFloat(window.document.getElementById("pluri").value);
 
     var final = (np1+part1)*0.4 + ((np2*0.8+pluri*0.2)+part2)*0.6;
+
+    res.innerHTML = `Média das notas: <strong>${final}</strong>`;
     
-    return document.getElementById("res").innerHTML = "Média das notas: " + final;
 }
